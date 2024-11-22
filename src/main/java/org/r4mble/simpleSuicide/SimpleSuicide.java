81e5138befbe035a8f7b902c52a999d646a4e35d
R4mBLe_ <misharad2008@gmail.com>
2024-11-22 19:27:58 +0300

    v1.0.2

---
+++ src/main/java/org/r4mble/simpleSuicide/SimpleSuicide.java
@@ -0,0 +1,25 @@
+package org.r4mble.simpleSuicide;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.r4mble.simpleSuicide.commands.SuicideCommand;
+
+public final class SimpleSuicide extends JavaPlugin {
+
+    public static FileConfiguration config;
+    @Override
+    public void onEnable() {
+        getCommand("suicide").setExecutor(new SuicideCommand());
+        saveResource("config.yml", false);
+        saveDefaultConfig();
+        config = getConfig();
+        getComponentLogger().info(Component.text("Configuration is loaded."));
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/org/r4mble/simpleSuicide/commands/SuicideCommand.java
@@ -0,0 +1,32 @@
+package org.r4mble.simpleSuicide.commands;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.r4mble.simpleSuicide.SimpleSuicide;
+
+import static org.r4mble.simpleSuicide.SimpleSuicide.config;
+
+public class SuicideCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if(!(commandSender instanceof Player)) return false;
+        Player player = (Player) commandSender;
+        if (!player.hasPermission("SimpleSuicide.suicide")) {
+            player.sendMessage(config.getString("messages.no-permission"));
+            return true;
+        }
+        player.setHealth(0.0);
+        if (SimpleSuicide.config.getBoolean("allow-messages.local-message-allow"))
+            player.sendMessage(config.getString("messages.local-message"));
+        if (SimpleSuicide.config.getBoolean("allow-messages.global-message-allow")) {
+            String msg=config.getString("messages.global-message").replace("%player%", player.getName());
+            player.getServer().sendMessage(Component.text(msg));
+        }
+
+        return true;
+    }
+}
